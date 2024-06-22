52638627d781f842213fb36f48e91bfbef74df41
Hernan Diaz <hernandiaz035@gmail.com>
2024-06-22 17:05:15 +1000

    Basic endpoint example

--- lib/cdk-lambda-stack.ts
+++ lib/cdk-lambda-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import path = require("path");
@@ -9,20 +9,24 @@ export class CdkLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
     const getSignedUrl = new NodejsFunction(this, "getSignedURL", {
       runtime: Runtime.NODEJS_20_X,
       entry: path.join(__dirname, "../src/get-signed-url.ts"),
       handler: "handler",
       environment: {
-        MESSAGE: "Hello World!"
-      }
+        SIGNING_KEY: "fjf4pw^qek$3*@9p",
+      },
+    });
+
+    const getSignedUrlEndpoint = getSignedUrl.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "getSignedUrlEndpoint", {
+      value: getSignedUrlEndpoint.url,
     });
   }
 }
--- src/get-signed-url.ts
+++ src/get-signed-url.ts
@@ -1,14 +1,34 @@
 import { Handler } from "aws-lambda";
 
 export const handler: Handler = async (event, context) => {
-  const method = event.requestContext.http.method;
+  const method = event.requestContext.http.method as string;
+  if (method === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    };
+  }
+
+  if (method !== "GET") {
+    return {
+      statusCode: 405,
+      body: {
+        message: "Method Not Allowed",
+      },
+    };
+  }
+
+  const token = event.queryStringParameters?.token;
 
   return {
     statusCode: 200,
     body: {
-      message: process.env.MESSAGE,
-      method,
-      context,
+      signingKey: process.env.SIGNING_KEY,
+      token,
     },
   };
 };
